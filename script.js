157887863e3eb847b99cfada1bb199ae44895e94
Fita04 <crackers0404@gmail.com>
2026-04-24 17:31:27 +0800

    Styled it to resemble an actual calculator. Added querySelectors for the calculator-screen and calculator-buttons divs. Added labels that will be used for each button Changed the styling so that the calculator resembled an actual calculator. I might change the color of the buttons section of the calc but I'll keep it like this for now to make it disntinct from the body. Also added querySelectors for the screen and buttons divs but haven't done anything with it. Also made the array buttonLabels, which contains strings that will be used to label each button on the calculator. Will probably make a leftSide and rightSide version of each but for now I only made one. I arranged the array in a way that resembles the layout of an actual calculator so that visualization of the end-product is easier.

---
+++ script.js
@@ -0,0 +1,10 @@
+const calculatorScreen = document.querySelector(".calculator-screen");
+const calculatorButtons = document.querySelectorAll(".calculator-buttons");
+
+let buttonLabels = [
+    "7", "8", "9",
+    "4", "5", "6",
+    "1", "2", "3",
+    "0", ".", 
+];
+
