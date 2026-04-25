c27e6eee9a16c5ff66e51be6629958cc2546ada5
Fita04 <crackers0404@gmail.com>
2026-04-26 01:37:10 +0800

    Added buttons to the calculator Added the buttons to the calculator. Did this by making two arrays, one for left-side buttons and one for right-side, and using a forEach loop to make appropriately labeled buttons. Still haven't increased its size nor added event listeners to it. Will do that in the future.

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const calculatorScreen = document.querySelector(".calculator-screen");
-const calculatorButtons = document.querySelectorAll(".calculator-buttons");
+const leftSideButtons = document.querySelector(".left-side-buttons");
+const rightSideButtons = document.querySelector(".right-side-buttons");
 
-let buttonLabels = [
+const buttonLabelsLeft = [
     "7", "8", "9",
     "4", "5", "6",
     "1", "2", "3",
     "0", ".", 
 ];
 
+buttonLabelsLeft.forEach(label => {
+    button = document.createElement("button");
+    button.className = "button";
+    button.textContent = label;
+    leftSideButtons.appendChild(button);
+}); 
+
+const buttonLabelsRight = [
+    "Del", "CE",
+    "X", "/",
+    "+", "-",
+    "=",
+];
+
+buttonLabelsRight.forEach(label => {
+    button = document.createElement("button");
+    button.className = "button";
+    button.textContent = label;
+    rightSideButtons.appendChild(button);
+}); 
